6729243991541d002ed628539300b0a7a5a06be5
Gabriel Prates Bitencourt <devprates@gmail.com>
2024-03-02 10:55:16 -0400

    Starting code

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <h1>Home page</h1>
+      <Button>Button</Button>
+
+    </main>
+  );
+}

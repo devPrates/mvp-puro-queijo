645239c0f6a93731f8bab336f1c37a06ac76dacb
Gabriel Prates Bitencourt <devprates@pmnavirai.local>
2024-03-08 10:38:32 -0400

    project struture

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,7 @@
+const Layout = ({ children }) => {
+    return (
+        <h1>{children}</h1>
+    )
+}
+
+export default Layout
---
+++ app/dashboard/users/page.jsx
@@ -0,0 +1,9 @@
+const Users = () => {
+    return (
+        <h1>
+            pagina de usuarios
+        </h1>
+    )
+}
+
+export default Users

a3613bbe774b3c53840b9b9d0a8b6edf7fc82bac
Gabriel Prates Bitencourt <devprates@gmail.com>
2024-03-20 16:49:36 -0400

    Hero section bug resolution

--- components/hero/page.jsx
+++ components/hero/page.jsx
@@ -11,7 +11,7 @@ import BadgeHero from "./Badge"
 
 const Hero = () => {
     return (
-        <section className="py-12 xl:py:24 h-[84vh] xl:pt-28 bg-orange-300 dark:bg-gray-800">
+        <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
             <div className="container mx-auto">
                 <div className="flex justify-between gap-x-8">
                     {/* text */}
@@ -59,12 +59,12 @@ const Hero = () => {
                             endCountText='K'
                             badgeText='Clientes'
                         />
-                        <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2" >
+                        <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2" ></div>
                             <HeroImage 
                                 imgSrc='/hero/developer.png'
                                 containerStyles='bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom'
                             />
-                        </div>
+                        
                     </div>
                 </div>
                 <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
